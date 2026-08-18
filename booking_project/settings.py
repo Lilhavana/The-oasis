35d0807e915e7302aea265806a1f6bd7e8533a67
Osim Emmanuel <osimemmanuel2@gmail.com>
2026-08-18 20:11:22 +0100

    Last changes

--- booking_project/settings.py
+++ booking_project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'booking_project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
+        default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
     )
 }
